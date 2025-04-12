f4e023a825beaa785258badc1bcca38f9f0cda8a
Roohith Bala G <roohithbalag.23csd@kongu.edu>
2025-04-12 15:55:16 +0530

    Delete s1.py

--- s1.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-with db.engine.connect() as connection:
-    connection.execute("DROP TABLE IF EXISTS book;")
-    connection.commit()
-
-print("Book table dropped successfully!")
